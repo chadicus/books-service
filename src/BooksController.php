811e5ded928a91a4c75dc65ef553bdc9aff31ab1
chadicus <chadwickgray@gmail.com>
2016-09-13 16:08:33 -0400

    Initial commit

---
+++ src/BooksController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Chadicus;
+
+use Chadicus\Books;
+use DominionEnterprises\Util\Arrays;
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\Stream;
+
+final class BooksController
+{
+    private $repository;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->repository = $container->get('books-respository');
+    }
+
+    public function index(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        $queryParams = $request->getQueryParams();
+        $limit = Arrays::get($queryParams, 'limit', 5);
+        $offset = Arrays::get($queryParams, 'offset', 0);
+
+        $books = $this->repository->findAll(new Books\BookCriteria());
+        $total = count($books);
+        $result = [
+            'offset' => $offset,
+            'limit' => min($limit, $total),
+            'total' => $total,
+            'books' => array_slice($books, $offset, $limit),
+        ];
+
+        $stream = fopen('php://temp', 'r+');
+        fwrite($stream, json_encode($result));
+        rewind($stream);
+
+        return $response->withHeader('Content-Type', 'application/json')->withBody(new Stream($stream));
+    }
+
+    public function get(ServerRequestInterface $request, ResponseInterface $response, array $args = [])
+    {
+        $id = Arrays::get($args, 'id');
+
+        $book = $this->repository->find($id);
+        if ($book === null) {
+            return $response->withStatus(404);
+        }
+
+        $stream = fopen('php://temp', 'r+');
+        fwrite($stream, json_encode($book));
+        rewind($stream);
+
+        return $response->withHeader('Content-Type', 'application/json')->withBody(new Stream($stream));
+    }
+}
